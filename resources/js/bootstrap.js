97cb61413d5fab19d32cec589104f4080c7f39d4
Yassine Fariss <farissyassine8@gmail.com>
2026-07-13 08:23:46 +0100

    Fix user dropdown: add Bootstrap Dropdown JS import
    Only Toast and Collapse were imported, breaking the user menu dropdown.
    Added Dropdown import to bootstrap.js + rebuilt Vite assets.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,5 @@
 import Toast from 'bootstrap/js/dist/toast';
 import Collapse from 'bootstrap/js/dist/collapse';
+import Dropdown from 'bootstrap/js/dist/dropdown';
 
-window.bootstrap = { Toast, Collapse };
+window.bootstrap = { Toast, Collapse, Dropdown };
